2fed76306748edecd0f6a50515f8b2bb83ece20e
Divya Pyneni <d.pyneni@zensar.com>
2022-06-28 11:39:42 +0530

    Book management api

--- bookmanagementapi/routes/books.router.js
+++ bookmanagementapi/routes/books.router.js
@@ -9,7 +9,7 @@ router.get("/",authMiddleware,booksController.getAllBooks);
 router.get("/:id",authMiddleware,booksController.getBookById);
 router.post("/",authMiddleware,booksController.createBook);
 router.put("/:id",authMiddleware,booksController.updateBooks);
-router.delete("/:id",booksController.deleteBook);
+router.delete("/:id",authMiddleware,booksController.deleteBook);
 
 
 module.exports=router;
